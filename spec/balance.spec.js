3b3e41ad8f6a19f79f82683765abced0ef27d9bf
Dileep <dileepvunnam@gmail.com>
2023-07-01 18:49:13 +0100

    Test failed

--- spec/balance.spec.js
+++ spec/balance.spec.js
@@ -28,6 +28,27 @@ describe('Making a deposit', () => {
         const newBalance = account.getBalance();
         expect(newBalance).toBe(initialBalance + depositAmount);
     });
+
+    it('should throw an error if deposit amount exceeds the limit', () => {
+        //Arrange
+        const initialBalance = account.getBalance();
+        //Act
+        //Assert
+        expect(() => { account.deposit(100000) }).toThrowError('Deposit amount exceeds limit');
+        expect(account.getBalance()).toBe(initialBalance);
+    });
+    it('should throw an error if deposit amount is not greater than zero', () => {
+        // Arrange
+        const invalidDepositAmount = 0;
+
+        // Act and Assert
+        expect(() => {
+            account.deposit(invalidDepositAmount);
+        }).toThrowError('Invalid deposit amount');
+
+        expect(account.getBalance()).toBe(0);
+    });
+
 });
 
 
--- src/Account.js
+++ src/Account.js
@@ -1,13 +1,19 @@
 class Account {
     #balance
+    #depositLimit;
     constructor() {
         this.#balance = 0
+        this.#depositLimit = 5000;
     };
     getBalance() {
         return this.#balance;
     };
     deposit(value) {
-        this.#balance += value;
+        if (value < this.#depositLimit) {
+            this.#balance += value;
+        } else {
+            throw new Error('Deposit amount exceeds limit');
+        }
     }
 }
 export default Account;
